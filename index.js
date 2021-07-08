07556c12b8a20fda8f5bc61b3e54cdd67487af7f
Ethan Edmond <eedmond@uccs.edu>
2021-07-08 09:01:28 -0600

    Console logging port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 
+console.log(process.env.PORT);
+
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
